e5b367637bb0dd13a54e7e91e08d638e0b981746
Isabella <wileyisabella@gmail.com>
2023-03-29 22:37:51 -0400

    disabled debug in backend

--- vet-portal-api/base.py
+++ vet-portal-api/base.py
@@ -62,4 +62,4 @@ def home():
     return jsonify(response_body)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=False)
